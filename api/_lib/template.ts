467089dcf31f847979ad4b949e494194f1621adb
shreyfirst <shreysgupta@gmail.com>
2021-03-06 18:28:46 -0800

    Update template.ts

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -158,8 +158,8 @@ export function getHtml(parsedReq: ParsedRequest) {
   </style>
   <body>
     <div class="brand">
-      <img class="logo" src="https://github.com/lachlanjc.png">
-      <span>@lachlanjc/</span>notebook
+      <img class="logo" src="https://scontent-lax3-2.cdninstagram.com/v/t51.2885-19/s320x320/141109163_3941240225889046_6066970745713957948_n.jpg?tp=1&_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_ohc=YSndtVuKH7EAX_yFW72&oh=4dcdf4776a8481c1a2af76619a57f276&oe=606D49F8">
+      Vote&nbsp;<span>Shrey&nbsp;/</span>for&nbsp;IHS&nbsp;ASB&nbsp;President
     </div>
     <div class="container">
       ${
